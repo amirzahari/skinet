924f324f3405621302316b0c64041333df3e2a91
Amir Zahari <amirzahari91@gmail.com>
2023-01-18 16:00:16 +0800

    End of section 5

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,54 @@
+using API.Errors;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly StoreContext _context;
+
+        public BuggyController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        #region ErrorController
+
+        [HttpGet("notfound")]
+        public ActionResult GetNotFoundRequest()
+        {
+            var thing = _context.Products.Find(-21);
+            if (thing == null)
+            {
+                return NotFound(new ApiResponse(404));
+            }
+            return Ok();
+        }
+
+        [HttpGet("servererror")]
+        public ActionResult GetServerError()
+        {
+            // not found in DB. and null bind to string
+            var thing = _context.Products.Find(-21);
+            var thingToReturn = thing.ToString();
+            return Ok();
+        }
+
+        [HttpGet("badrequest")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest(new ApiResponse(400));
+        }
+
+        [HttpGet("badrequest/{id}")]
+        public ActionResult GetNotFoundRequest(int id)
+        {
+            // will send string instead of int for id
+            return Ok();
+        }
+
+        #endregion
+    
+    }
+}   
---
+++ API/Errors/ApiResponse.cs
@@ -0,0 +1,27 @@
+namespace API.Errors
+{
+    public class ApiResponse
+    {
+        public ApiResponse(int statusCode, string message = null)
+        {
+            StatusCode = statusCode;
+            Message = message ?? GetDefaultMessageForStatusCode(statusCode);
+        }
+
+        public int StatusCode { get; set; }
+
+        public string Message { get; set; }
+
+        private string GetDefaultMessageForStatusCode(int statusCode)
+        {
+            return statusCode switch
+            {
+                400 => "A Bad Request, you have made",
+                401 => "Authorized, you are not",
+                404 => "Resource Found, it was not",
+                500 => "Internal Server ERRORRORR",
+                _ => null
+            };
+        }
+    }
+}
