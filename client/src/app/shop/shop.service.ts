7420217cae3f6da0a9cc891cd9f2eac0826c72c6
Amir Zahari <amirzahari91@gmail.com>
2023-02-24 01:39:29 +0800

    End of section 10

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -25,7 +25,11 @@ export class ShopService {
     params = params.append('pageSize', shopParams.pageSize);
     if (shopParams.search) params = params.append('search', shopParams.search);
 
-    return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products', {params: params});
+    return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products', { params: params });
+  }
+
+  getProduct(id: number) {
+    return this.http.get<Product>(this.baseUrl + 'products/' + id)
   }
 
   getBrands() {
